744a93c10bd739cf48398c7169da031d6a65c6ce
Emily Braman <embraman10@gmail.com>
2020-06-16 15:42:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, index), results|
     index.each do |value, names|
       names.each do |name|
-        results[name]= {}
-        results[name][value]= []
+        results[name] ||= {}
+        results[name][value] ||= []
         results[name][value] << value.to_s
       end
     end
